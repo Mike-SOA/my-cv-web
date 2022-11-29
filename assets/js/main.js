8b7291461c55cee8092859cc1003d30808cfd3ef
Mykhailo Kushnir <Anarchyrock657@gmail.com>
2022-11-29 22:16:11 +0200

    add media and burger

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,10 @@ $(document).ready(function () {
     arrows: true,
   });
 });
+
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
